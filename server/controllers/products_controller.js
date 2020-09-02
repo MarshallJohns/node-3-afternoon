f0ccfc43bb63d802d0fb77d39bdb336beabf4b7e
Marshall Johnson <marshalljohnson193@gmail.com>
2020-09-02 16:50:09 -0600

    finished

--- server/controllers/products_controller.js
+++ server/controllers/products_controller.js
@@ -7,17 +7,53 @@ module.exports = {
             res.status(200).send(product)
         }).catch(err => {
             res.status(500).send('Could not create produt')
+            console.log(err.message)
         })
     },
     getOne: (req, res) => {
         const db = req.app.get('db')
-        const { product_id } = req.body
+        const { product_id } = req.params
 
-        db.read_product([product_id]).then(product => {
+        db.read_product(product_id).then(product => {
             res.status(200).send(product)
+        }).catch(err => {
+            res.status(500).send('Could not find product')
+            console.log(err.messsage)
         })
     },
-    getAll: (req, res) => { },
-    update: (req, res) => { },
-    delete: (req, res) => { }
+    getAll: (req, res) => {
+        const db = req.app.get('db')
+
+        db.read_products().then(products => {
+            res.status(200).send(products)
+        }).catch(err => {
+            res.send(500).send('There was an issue getting products')
+            console.log(err.message)
+        })
+    },
+    update: (req, res) => {
+        const db = req.app.get('db')
+        const { product_id } = req.params
+        const { desc } = req.query
+
+        console.log(desc)
+        db.update_product([product_id, desc]).then(products => {
+            console.log(products)
+            res.status(200).send(products)
+        }).catch(err => {
+            res.status(500).send('Could not update product')
+            console.log(err.message)
+        })
+    },
+    delete: (req, res) => {
+        const db = req.app.get('db')
+        const { product_id } = req.params
+
+        db.delete_product([product_id]).then(products => {
+            res.status(200).send(products)
+        }).catch(err => {
+            res.status(500).send(`Could not delete product`)
+            console.log(err.message)
+        })
+    }
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
+const productCtrl = require('./controllers/products_controller')
 
 const app = express()
 const { SERVER_PORT, CONNECTION_STRING } = process.env
 
 app.use(express.json())
+
+app.get('/api/products', productCtrl.getAll)
+app.get('/api/products/:product_id', productCtrl.getOne)
+app.post('/api/products', productCtrl.create)
+app.put('/api/products/:product_id', productCtrl.update)
+app.delete('/api/products/:product_id', productCtrl.delete)
+
 
 massive({
     connectionString: CONNECTION_STRING,
